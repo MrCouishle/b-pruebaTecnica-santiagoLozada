4837d8ead2b83eefa62c2c4d744ed87c9afd07ae
Santiago Lozada <100322634+MrCouishle@users.noreply.github.com>
2025-04-09 17:13:50 -0500

    [Feat] Add CurrentBalance column to Result table and update DataContext model snapshot

---
+++ Migrations/20250409164406_04.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Roulette.Migrations
+{
+    /// <inheritdoc />
+    public partial class _04 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "CurrentBalance",
+                table: "Result",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentBalance",
+                table: "Result");
+        }
+    }
+}
